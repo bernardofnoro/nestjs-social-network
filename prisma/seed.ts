3e8a7f1f45753da5fe73faaddbc7a6e0ea00b8fa
chanelym <chanelym@gmail.com>
2022-01-05 21:50:28 -0300

    (feat): seed file test

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,8 +2,21 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-async function main() { 
+const tweets: any = [
+  'Eu vou enfiar uva no céu da sua boca',
+  'Quem ganhou tem que levar. Leva essa droga pra lá',
+  'Porque sei que tudo isso passa. Você me abraça e a gente se ama',
+  'Amor demais, lembro dos momentos que te amei demais'
+];
 
+async function main() { 
+  for (const tweet of tweets) {
+    const createTweet = prisma.tweet.create({
+      data: {
+        content: tweet,
+      },
+    });
+  }
 }
 
 main()
